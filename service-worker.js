8233e8187a1d70ae2e0b523b5c768e7edae57aa0
trezevga <gardner.trezevant1@gmail.com>
2025-08-11 01:06:54 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('pd-v6').then(c=> c.addAll(['./','./index.html','./styles.css','./manifest.webmanifest'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', (e)=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('pd-v7').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'])));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
